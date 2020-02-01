bf94823e40f42759e74f08873ffa54034a548f21
Samuel-Downs <SamuelDowns17@gmail.com>
2020-02-01 16:21:40 -0500

    added oxygen bar2

---
+++ GGJ2020/Assets/Oxygen/script/GameHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    public float oxygenDepletion = 0.005f;
+    [SerializeField] private OxygenBar oxygenBar;
+    private void Start()
+    {
+        //oxygenBar.SetSize(.4f);
+        Debug.Log("this has happend");
+        float tank = 1f;
+        FunctionPeriodic.Create(() =>
+        {
+            if (tank >= oxygenDepletion)
+            {
+                tank -= oxygenDepletion;
+                oxygenBar.SetSize(tank);
+                if (tank < .3f)
+                {
+                    if (((int)(tank * 100) * 100) % 3 == 0)
+                    {
+                        oxygenBar.SetColor(Color.blue);
+                    }
+                    else {
+                        oxygenBar.SetColor(Color.white); }
+                }
+            }
+        }, 0.2f);
+        //oxygenBar.SetColor(Color.white);
+    }
+
+
+}
---
+++ GGJ2020/Assets/Oxygen/script/OxygenBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenBar : MonoBehaviour {
+   
+    //private Transform bar;
+    GameObject bar;
+    GameObject BarSprite;
+
+    private void Start()
+    {
+       bar =  GameObject.Find("Bar");
+       BarSprite = GameObject.Find("BarSprite");
+       //bar.transform.localScale = new Vector3(0, 0,0);
+       //bar = transform.Find("bar");
+       //bar.localScale = new Vector3(0.5f, 1, 1);
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.transform.localScale = new Vector3(sizeNormalized, 1f,0);
+    }
+    public void SetColor(Color color)
+    {
+        BarSprite.GetComponent<SpriteRenderer>().color=color;
+    }
+
+
+}
