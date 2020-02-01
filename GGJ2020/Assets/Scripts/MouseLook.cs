9f81b2d0f974d5ddc7d876c68c586d6597b5cde3
Justin Sandman <supersand24@gmail.com>
2020-02-01 03:00:42 -0500

    Started work on selection.

---
+++ GGJ2020/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    public Texture cursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void OnGUI()
+    {
+        float cursorWidth = 10;
+        float cursorHeight = 10;
+        GUI.DrawTexture(new Rect((Screen.width / 2) - (cursorWidth / 2), (Screen.height / 2) - (cursorHeight / 2), cursorWidth, cursorHeight), cursor);
+    }
+}
---
+++ GGJ2020/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+    public Transform holding = null;
+
+    private Transform _selection;
+
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Button Clicked");
+                    holding = selection;
+                }
+            }
+        }
+
+        if (holding != null)
+        {
+            holding.position = new Vector3(GameObject.Find("Hand").transform.position.x, GameObject.Find("Hand").transform.position.y, GameObject.Find("Hand").transform.position.z);
+        }
+
+    }
+
+}
