ba8975da9c1e41668f922626c4da8ef723d3c5d4
ndonga1 <nevildonga2000@gmail.com>
2020-02-01 13:21:32 -0500

    made some changes

---
+++ GGJ2020/Assets/gravity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour
+{
+    public bool gravityOn = true;
+    GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collision detected");
+
+        if (other.tag == "player")
+        {
+            gravityOn = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gravityOn == false)
+        {
+           playerMovement move = player.GetComponent<playerMovement>();
+            // gravity.player = 0;
+            move.GravityEnabled;
+        }
+    }
+}
