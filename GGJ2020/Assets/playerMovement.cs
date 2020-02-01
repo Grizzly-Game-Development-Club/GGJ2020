2305b7890dc25e5d254ca862ff327f7d620b64c7
Justin Sandman <supersand24@gmail.com>
2020-01-31 22:09:15 -0500

    Added Movement

--- GGJ2020/Assets/playerMovement.cs
+++ GGJ2020/Assets/playerMovement.cs
@@ -2,48 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    Vector3 rot;
-    private Rigidbody rigid;
-    public float maxSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigid = GetComponent<Rigidbody>();
-    }
+    public CharacterController controller;
 
-    // Update is called once per frame
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform gravityCheck;
+    public float gravityDistance = 0.4f;
+    public LayerMask gravityMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(gravityCheck.position, gravityDistance, gravityMask);
 
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        float mx = Input.GetAxis("Mouse X");
-        float my = Input.GetAxis("Mouse Y");
-
-        Vector3 rotStep = Vector3.up * mx + Vector3.left * my;
-        //get total rotation
-        rot += rotStep * 360 * Time.deltaTime;
-        //limit rotation around x
-        rot.x = Mathf.Clamp(rot.x, -15, 15);
-        //apply to camera
-        transform.eulerAngles = rot;
-
-        //Move Forwards
-        if (Input.GetKey("w"))
+        if (isGrounded && velocity.y < 0)
         {
-            rigid.velocity += new Vector3(0, 0, transform.forward.z);
-            Mathf.Clamp(rigid.velocity.x, maxSpeed * -1, maxSpeed);
-            Mathf.Clamp(rigid.velocity.y, maxSpeed * -1, maxSpeed);
-            Mathf.Clamp(rigid.velocity.z, maxSpeed * -1, maxSpeed);
+            velocity.y = -2f;
         }
-        else if (Input.GetKeyUp("w"))
-        {
-            rigid.velocity = new Vector3(0, 0, 0);
-        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
-
 }
