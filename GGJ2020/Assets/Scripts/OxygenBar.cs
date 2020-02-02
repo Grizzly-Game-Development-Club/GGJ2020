cceddc9e64d7f6a93743ef5346d0d118865d553d
Samuel-Downs <SamuelDowns17@gmail.com>
2020-02-02 00:10:06 -0500

    Oxygen Bar

---
+++ GGJ2020/Assets/Scripts/OxygenBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenBar : MonoBehaviour
+{
+    GameObject bar;
+    void Start()
+    {
+         bar = GameObject.Find("Bar");
+    }
+    public void SetSize(float sizeNormalized)
+    {
+        bar.transform.localScale = new Vector3(sizeNormalized, 1f, 0f);
+    }
+    public void SetColor(Color color)
+    {
+        GameObject.Find("barsprite").GetComponent<SpriteRenderer>().color = color;
+    }
+
+
+}
